13c8024cd3e234c5021dbf92a2767a3fcf4ba574
Diego Doná <diedona@gmail.com>
2022-03-11 21:03:49 -0300

    auth controller

---
+++ src/WebApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.ViewModels.Login;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody]LoginRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
---
+++ src/WebApi/ViewModels/Login/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.ViewModels.Login
+{
+    public class LoginRequest
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
