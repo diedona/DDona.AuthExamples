2a5928d1e5cb982fbe5569e1c4e2a39388a84ec3
Diego Doná <diedona@gmail.com>
2022-03-13 11:09:17 -0300

    inactivated at

--- src/Domain/Entities/UserEntity.cs
+++ src/Domain/Entities/UserEntity.cs
@@ -5,12 +5,15 @@ namespace Domain.Entities
 {
     public class UserEntity : BaseEntity
     {
+        private const string ADMIN_ROLE = "admin";
+
         public string Username { get; private set; }
         [JsonIgnore]
         public string Password { get; private set; }
         public DateTime DateOfBirth { get; private set; }
         public string Role { get; private set; }
         public string? Claims { get; private set; }
+        public DateTime? InactivatedAt { get; private set; }
 
         public UserEntity(Guid id, string username, string password, DateTime dateOfBirth, string role, string? claims) : base(id)
         {
@@ -19,6 +22,21 @@ namespace Domain.Entities
             DateOfBirth = dateOfBirth;
             Role = role;
             Claims = claims;
+        }
+
+        public bool IsAdmin()
+        {
+            return Role.Equals(ADMIN_ROLE);
+        }
+
+        public void InactivateUser()
+        {
+            this.InactivatedAt = DateTime.Now;
+        }
+
+        public bool IsUserActive()
+        {
+            return (!this.InactivatedAt.HasValue);
         }
 
     }
---
+++ src/Infrastructure/Migrations/20220313140824_user_inactivated-at.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class user_inactivatedat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8947de4d-069b-42fa-95a2-4b86462a816b"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "InactivatedAt",
+                table: "User",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "Claims", "DateOfBirth", "InactivatedAt", "Password", "Role", "Username" },
+                values: new object[] { new Guid("ce43ad34-9067-4156-a4fc-f1d45d4565d2"), null, new DateTime(1999, 2, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "$2a$11$iUXjk5CeNVyjWt7iEux/4ObazXXoKkStevbnJsoFAe7gOe9VeeQfi", "admin", "diego.dona" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("ce43ad34-9067-4156-a4fc-f1d45d4565d2"));
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedAt",
+                table: "User");
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "Claims", "DateOfBirth", "Password", "Role", "Username" },
+                values: new object[] { new Guid("8947de4d-069b-42fa-95a2-4b86462a816b"), null, new DateTime(1999, 2, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), "$2a$11$pJganvm9VxDwyafMZ/yvPOrsUOmdEVF/7OcPydvBuWBn9c9v89Ig.", "admin", "diego.dona" });
+        }
+    }
+}
