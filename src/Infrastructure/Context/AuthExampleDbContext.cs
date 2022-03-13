b2b40a1741a6986066ec9fe7b0c2ea76aa2ac8a5
Diego Doná <diedona@gmail.com>
2022-03-12 23:55:22 -0300

    add initial migration

--- src/Infrastructure/Context/AuthExampleDbContext.cs
+++ src/Infrastructure/Context/AuthExampleDbContext.cs
@@ -1,11 +1,21 @@
-﻿using Domain.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Context
 {
     public class AuthExampleDbContext : DbContext
     {
         public AuthExampleDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Properties<string>()
+                .HaveMaxLength(400);
+
+            configurationBuilder.Properties<string?>()
+                .HaveMaxLength(400);
+
+            base.ConfigureConventions(configurationBuilder);
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ src/Infrastructure/Migrations/20220313025236_initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: false),
+                    Claims = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
